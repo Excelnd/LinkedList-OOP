1d1b1d8c8fea90e12d495f0786889577c2d34538
Excelnd <ihsanmpm@gmail.com>
2018-06-12 19:52:37 +0530

    Add project files.

---
+++ LinkedList OOP/List.cpp
@@ -0,0 +1,81 @@
+#include "stdafx.h"
+#include <iostream>
+
+#include "List.h"
+
+using namespace std;
+
+List::List()
+{
+	head = NULL;
+	curr = NULL;
+	temp = NULL;
+}
+
+
+void List::AddNode(int addData)
+{
+	nodePtr n = new node;
+	n->next = NULL;
+	n->data = addData;
+
+	if (head != NULL)
+	{
+		curr = head;
+		while (curr->next != NULL)
+		{
+			curr = curr->next;
+		}
+		curr->next = n;
+	}
+	else
+	{
+		head = n;
+	}
+}
+
+void List::DeleteNode(int delData)
+{
+	nodePtr delPtr = NULL;
+	temp = head;
+	curr = head;
+	while (curr != NULL && curr->data != delData)
+	{
+		temp = curr;
+		curr = curr->next;
+	}
+	if (curr == NULL)
+	{
+		cout << delData << " was not in the list\n";
+	}
+	else
+	{
+		delPtr = curr;
+		curr = curr->next;
+		temp->next = curr;
+		if (delPtr == head)
+		{
+			head = head->next;
+			temp = NULL;
+		}
+		delete delPtr;
+		cout << "The value " << delData << " was deleted\n";
+	}
+}
+
+void List::PrintList()
+{
+	cout << "=========================\n";
+	curr = head;
+	while (curr != NULL)
+	{
+		cout << curr->data << endl;
+		curr = curr->next;
+	}
+	cout << "=========================\n";
+}
+
+
+List::~List()
+{
+}
---
+++ LinkedList OOP/List.h
@@ -0,0 +1,32 @@
+#include "stdafx.h"
+#include <iostream>
+
+
+using namespace std;
+
+class List
+{
+private:
+
+	typedef struct node {
+		int data;
+		node* next;
+	} * nodePtr;
+
+	nodePtr head;
+	nodePtr curr;
+	nodePtr temp;
+
+public:
+	List();
+	void AddNode(int addData);
+	void DeleteNode(int delData);
+	void PrintList();
+	
+
+
+
+
+	~List();
+};
+
